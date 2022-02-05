6b0229d532a29db02aac94b2831964884ffaa794
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-06 00:02:28 +0400

    Add a SwaggerDoc option to Program.cs

--- src/TaskManager.Api/Program.cs
+++ src/TaskManager.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new OpenApiInfo()
+	{
+		Version = "v1",
+		Title = "Task Manager API",
+		Description = "An ASP.NET Core Web API for managing Tasks",
+	});
+});
 
 var app = builder.Build();
 
