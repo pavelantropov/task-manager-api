f3d93ecfcdf44a2b30e804e884373c91f2a1ed7e
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-05 23:54:10 +0400

    Update TasksController

--- src/TaskManager.Api/Controllers/TasksController.cs
+++ src/TaskManager.Api/Controllers/TasksController.cs
@@ -1,6 +1,21 @@
-﻿namespace Antropov.TaskManager.Api.Controllers;
+﻿using Antropov.TaskManager.Api.ApiModels;
+using Antropov.TaskManager.Api.Models;
+using Antropov.TaskManager.Api.Services;
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Antropov.TaskManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
 public class TasksController
 {
-	
+	private readonly ITaskService _service;
+
+	public TasksController(ITaskService service)
+	{
+		_service = service;
+	}
 }
