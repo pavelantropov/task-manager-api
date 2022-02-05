2639f87a38d799c87e86e2aa4606a0b245e9055c
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-05 23:58:48 +0400

    Add an empty CreateTask method to TaskService

--- src/TaskManager.Api/Services/TaskService.cs
+++ src/TaskManager.Api/Services/TaskService.cs
@@ -1,6 +1,13 @@
-﻿namespace Antropov.TaskManager.Api.Services;
+﻿using System;
+
+using Antropov.TaskManager.Api.Models;
+
+namespace Antropov.TaskManager.Api.Services;
 
 public class TaskService : ITaskService
 {
-	
+	public void CreateTask(Task task)
+	{
+		throw new NotImplementedException();
+	}
 }
