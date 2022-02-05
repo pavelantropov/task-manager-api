2713d61fc23eaac7f351c60bc660b37c49e0326f
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-05 23:53:30 +0400

    Create the Task model

--- src/TaskManager.Api/Models/Task.cs
+++ src/TaskManager.Api/Models/Task.cs
@@ -1,6 +1,10 @@
-﻿namespace Antropov.TaskManager.Api.Models;
+﻿using System;
+
+namespace Antropov.TaskManager.Api.Models;
 
 public class Task
 {
-	
+	public string? Title { get; set; }
+	public string? Description { get; set; }
+	public DateTime? Deadline { get; set; }
 }
