1d98dac354f7fb9b6ac9ae1581af2324ea3f02c7
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-05 23:34:44 +0400

    Create a project for the API tests

---
+++ src/TaskManager.Api.Tests/TaskControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Antropov.TaskManager.Api.Tests
+{
+	public class TaskControllerTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
