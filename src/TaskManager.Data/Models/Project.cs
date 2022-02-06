9b6df3a54dbf429849d9feca42f35062ffb76e6f
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-06 15:16:47 +0400

    Create the Project model

---
+++ src/TaskManager.Data/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Antropov.TaskManager.Data.Models;
+
+public class Project
+{
+	public int ProjectId { get; set; }
+	public string? Title { get; set; }
+	public string? Description { get; set; }
+	public DateTime? Deadline { get; set; }
+
+	public List<TaskObject>? Tasks { get; set; }
+}
