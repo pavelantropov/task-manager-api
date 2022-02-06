67743bd3a4b71366c6af6a51207240091173f6de
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-06 15:17:46 +0400

    Create a static class with a default tasks list as a stub for the future database

---
+++ src/TaskManager.Data/Data.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Antropov.TaskManager.Data.Models;
+
+namespace Antropov.TaskManager.Data;
+
+// Replace with a database
+public static class Data
+{
+	public static List<TaskObject>? Tasks { get; }
+
+	static Data()
+	{
+		Tasks = new List<TaskObject>
+		{
+			new ()
+			{
+				TaskId = 1,
+				Title = "Complete AZ-900 MS learning path",
+				Description = "Complete the remaining modules. Start preparing for the exam for real",
+				Deadline = new DateTime(2022, 02, 07),
+			},
+			new ()
+			{
+				TaskId = 2,
+				Title = "Get rid of the intern status",
+				Description = "Become a real Junior Software Engineer finally. Get a 3x raise",
+				Deadline = new DateTime(2022, 02, 16),
+			},
+			new ()
+			{
+				TaskId = 3,
+				Title = "Credit card payment",
+				Description = "Find ₽110,000 and deposit to the Sberbank credit card",
+				Deadline = new DateTime(2022, 02, 28),
+			},
+		};
+	}
+}
