dfee1cb1cbaa5ecb5cfbf14ba73544bc5529b8a3
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-06 15:35:40 +0400

    Add a repository field to TaskService

--- src/TaskManager.Api/Services/TaskService.cs
+++ src/TaskManager.Api/Services/TaskService.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 
 using Antropov.TaskManager.Data.Models;
+using Antropov.TaskManager.Data.Repositories;
 
 namespace Antropov.TaskManager.Api.Services;
 
 public class TaskService : ITaskService
 {
+	private readonly ITaskRepository _repository;
+
+	public TaskService(ITaskRepository repository)
+	{
+		_repository = repository;
+	}
+
 	public List<TaskObject>? GetAllTasks()
 	{
 		throw new NotImplementedException();
