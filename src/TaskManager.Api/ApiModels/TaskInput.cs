b70731c53c5d18c18e6201aa49df40dc2aabeb18
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-10 01:26:55 +0400

    Rename the input model

--- src/TaskManager.Api/ApiModels/TaskInput.cs
+++ src/TaskManager.Api/ApiModels/TaskInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Antropov.TaskManager.Api.ApiModels;
 
-public class CreateTaskInput
+public class TaskInput
 {
 	public string? Title { get; set; }
 	public string? Description { get; set; }
