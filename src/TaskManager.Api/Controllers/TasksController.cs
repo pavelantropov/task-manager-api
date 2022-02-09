f2af1436db30c3963e8f18eda436689bb534f564
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-02-10 01:27:28 +0400

    Create Update and Delete actions

--- src/TaskManager.Api/Controllers/TasksController.cs
+++ src/TaskManager.Api/Controllers/TasksController.cs
@@ -55,5 +55,47 @@ public class TasksController : Controller
 		};
 		await _service.CreateAsync(task);
 		return Ok(input);
+
+	[HttpPut("{id:length(24)}")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Update(string id, TaskInput input)
+	{
+		var task = await _service.GetAsync(id);
+
+		if (task is null)
+		{
+			return NotFound();
+		}
+
+		var updatedTask = new TaskObject
+		{
+			TaskId = task.TaskId,
+			Title = input.Title,
+			Description = input.Description,
+			Deadline = input.Deadline,
+			Labels = input.Labels,
+		};
+
+		await _service.UpdateAsync(id, updatedTask);
+
+		return Ok();
+	}
+
+	[HttpDelete("{id:length(24)}")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Delete(string id)
+	{
+		var task = await _service.GetAsync(id);
+
+		if (task is null)
+		{
+			return NotFound();
+		}
+
+		await _service.RemoveAsync(id);
+
+		return Ok();
 	}
 }
